503a71d7df84201f21c5e188879dff434830de1f
Thinh Vu <thinh.vu@seldatinc.com>
2017-04-12 23:47:59 +0700

    Update

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'first-app',
-    template: '<h1>HEHE</h1>'
+    template: `<h1>{{user?.firstname}} {{user.lastname}}</h1>
+    			<h4 [textContent]="user.firstname"></h4>
+    			<h4 bind-textContent="user.firstname"></h4>
+    			<h4 [innerHtml]="user.firstname"></h4>
+    			<h4 bind-innerHtml="user.firstname"></h4>`
+    			/*<h4 bind-name="user.firstname"></h4> go wrong bc h4 tag doesn't have name attribute as well as property
+    			<h4 [name]="user.firstname"></h4>`*/
 })
 
-export class FirstAppComponent{
+export class FirstAppComponent {
+
+	private user: any = {firstname: 'thinh', lastname: 'vu'};
     
-} 
+}
