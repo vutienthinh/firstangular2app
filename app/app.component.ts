1bbcba0cedb3980a4d67d4a9e18be963570cbbab
Thinh Vu <thinh.vu@seldatinc.com>
2017-04-12 12:00:13 +0700

    Init app

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'first-app',
+    template: '<h1>HEHE</h1>'
+})
+
+export class FirstAppComponent{
+    
+} 
---
+++ app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FirstAppComponent } from './app.component';
+
+@NgModule({
+	imports: [ BrowserModule ],
+	declarations: [ FirstAppComponent ],
+	bootstrap: [ FirstAppComponent ]
+})
+ 
+export class AppModule {
+    
+}
