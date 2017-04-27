d9a3b456a22bd65aefe8a9232482836a4f4bdef0
Thinh Vu <thinh.vu@seldatinc.com>
2017-04-27 19:16:17 +0700

    U

--- pages/observable/observable.component.ts
+++ pages/observable/observable.component.ts
@@ -9,9 +9,35 @@ import { Observable } from 'rxjs/Observable'
 })
 
 export class ObservableComponent {
-	observable: Observable<Array<any>>;
+	observeData: Observable<Array<any>>;
 	constructor() {
+		this.observeData = new Observable(observer => {
+			let count = 0;
 
+			setInterval(() => {
+				if(count < 6) {
+					observer.next(43434);
+					if(count == 3) {
+						observer.error(new Error('ERROR1'));
+					}
+					else if(count == 5) {
+						observer.complete();
+					}
+					count++;
+				}
+			}, 5000);
+		})
+		this.observeData.subscribe(
+			data => {
+				console.log(`data: ${data}`);
+			},
+			error => {
+				console.log(`error: ${error}`);
+			},
+			() => {
+				console.log(`complete`);
+			}
+		)
 		/*this.observable.([1,2,3,4,5])
 			.map(i => i * 4)
 			.subscribe(i => console.log(i));*/
